d2d75a128c2257566a452e2d098dce7e8a0e7cef
LIHAO <LIHAO@mac-pro.charter.com>
2016-03-17 20:54:52 -0700

    lat lng from double to String

--- src/cs320/controller/Search.java
+++ src/cs320/controller/Search.java
@@ -22,7 +22,7 @@ public class Search extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-  	
+    
         String latitude =  request.getParameter("lat"); // request.getParameter("latitude");
         String longitude = request.getParameter("lon"); // request.getParameter("longitude");
         String radius = request.getParameter("radius");
@@ -32,8 +32,8 @@ public class Search extends HttpServlet {
         request.setAttribute("radius", radius);
         
         if (radius == null) {
-			radius = "20";
-		}
+            radius = "20";
+        }
         
         if (latitude != null && longitude != null) {
             double lat = Double.parseDouble(latitude);
@@ -41,20 +41,18 @@ public class Search extends HttpServlet {
             double r = Integer.parseInt(radius) / 0.000621371192;  // Mile to meter
             
             String requestURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + 
-        			"location=" + lat + ",%20" + lng + 
-        			"&radius=" + r +
-        			"&key=AIzaSyDv_0sVA5OuZzQuulNUuP6gkYKMWt88vwk";
+                    "location=" + lat + ",%20" + lng + 
+                    "&radius=" + r +
+                    "&key=AIzaSyDv_0sVA5OuZzQuulNUuP6gkYKMWt88vwk";
             URL url = new URL(requestURL);
             Scanner scan = new Scanner(url.openStream());
             String jsonStr = new String();
             while (scan.hasNext())
-            	jsonStr += scan.nextLine();
+                jsonStr += scan.nextLine();
             scan.close();
             
             // build a JSON object
             JSONObject obj = new JSONObject(jsonStr);
-//            if (!obj.getString("status").equals("OK"))
-//                return;
             JSONArray anchors = obj.getJSONArray("results");
             ArrayList<POI> pois = new ArrayList<>();
             for (int i = 0; i < anchors.length(); i++) {
